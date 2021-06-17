e7126edd8007f803f91b7f48d21c9875cab39d87
Tania Mishakova <tmishakova123@gmail.com>
2021-06-17 18:25:27 +0200

    created currencies folder

---
+++ src/currencies/index.js
@@ -0,0 +1,7 @@
+const currencies = [
+    { id: "EUR", name: "Euro", rate: 4.5649 },
+    { id: "USD", name: "Dolar amerykański", rate: 3.7855 },
+    { id: "UAH", name: "Hrywna ukraińska", rate: 0.1356 },
+];
+
+export default currencies;
