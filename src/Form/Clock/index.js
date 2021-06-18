9b5e8c2e5442abedf1771796b7602ac3ab94c9f9
Tania Mishakova <tmishakova123@gmail.com>
2021-06-18 22:55:09 +0200

    creating structure for ShowCurrentDate komponent

---
+++ src/Form/Clock/index.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import "./style.css";
+
+export const ShowCurrentDate = () => {
+    const [date, setDate] = useState(new Date());
+    const formattedDate = date.toLocaleDateString(undefined, {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    });
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <p className="date">
+            Dzisiaj jest {formattedDate}
+        </p>
+    );
+};
