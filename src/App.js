b56c829e0c1324d56d91af3edb553fd6a694d581
Tania Mishakova <tmishakova123@gmail.com>
2021-06-24 13:16:36 +0200

    component Clock deleted from Form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Form from "./Form";
 import Table from "./Table";
 import currencies from "./currencies";
 import Container from "./Container";
+import { ShowCurrentDate } from "./Clock";
 import Footer from "./Footer";
 
 function App() {
   return (
     <Container>
       <Header title="Kalkulator walut online" />
+      <ShowCurrentDate />
       <Form currencies={currencies} />
       <Table currencies={currencies} />
       <Footer text=" &copy; Tania Mishakova 2021" />
